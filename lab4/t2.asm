2181a806199c4c737d425373c129e19ecdc6b67d
Mazurr <bartoszmazur98@gmail.com>
2020-10-20 14:29:23 +0200

    Better t2

--- lab4/t2.asm
+++ lab4/t2.asm
@@ -2,18 +2,11 @@
 CLK		EQU	P3.0
 ;Day light sensor input
 DYLG	EQU	P3.1
-;Light relay (active with a high state)
+
 LIGHT	EQU	P3.7
-;8 momentary push buttons (light switches)
+;8 momentary push buttons
 BT		EQU	P1
 
-; The controller remains inactive and turn-offs the light as long as
-; the daylight sensor is high. When the daylight sensor output goes low
-; initially the light is switched off. When any of the buttons is pressed the light is
-; switched on for 20 periods of the reference clock (rising edge). To make the system
-; energy efficient the light can be switched off by pushing the button again from
-; the 5th period of the reference clock. After turning off the light there is a gap
-; of 4 cycles before the light can be turned on again.
 
 DSEG	AT	30
 ;Timer variable
@@ -21,39 +14,42 @@ TMR:	DS	1
 
 CSEG	AT	0
 RESET:
-	MOV		SP, #7FH	
+	MOV		SP,#7FH	
 	
-;Your implementation goes here
+; Your implementation goes here	
 LIGHT_CTRL:
-    CLR		LIGHT	; Switching off
-    JB		DYLG, $ ; Controller inactive while daylight sensor is high
-    MOV     R0, BT
-    CJNE    R0, #0, LIGHT_ON ; Button pressed (manipulation by Keil I/O Ports utility)
-    SJMP    LIGHT_CTRL
-LIGHT_ON:
-    SETB    LIGHT
-    MOV 	R7, #5
-CYCLE5:
-    JNB		CLK, $
-    JB		CLK, $
-    DJNZ	R7, CYCLE5
-    MOV		R7, #15
-CYCLE15:
-    JNB		CLK, $
-    JB		CLK, $
-    CJNE	R0, #BT, NO_PUSH
-    SJMP	LIGHT_OFF
-NO_PUSH:
-    DJNZ    R7, CYCLE15
-LIGHT_OFF:
-    CLR		LIGHT
-    MOV		R7, #4
-GAP:
-    JNB		CLK, $
-    JB		CLK, $
-    DJNZ    R7, GAP
-    SJMP 	LIGHT_CTRL
+	CLR		LIGHT; Switching off
+	JB		DYLG,$
+	MOV		R0,BT
+	CJNE	R0,#0,LIGHT_ON
+	SJMP	Light_CTRL
+	Light_ON:
+	SetB	Light
+	Mov R7,#5
+	cycle5:
+	JNB		CLK,$
+	JB		CLK,$
+	DJNZ	R7,Cycle5
+	Mov	R7,#15
+	cycle15:
+	JNB		CLK,$
+	JB		CLK,$
+	Mov		A,R0
+	Subb	A,BT
+	JNZ no_push
+	SJMP	Light_off
+	no_push:
+	DJNZ	R7,cycle15
+	light_off:
+	CLR		light
+	MOV		R7,#4
+	Gap:
+	JNB		CLK,$
+	JB		CLK,$
+	DJNZ	R7,Gap
+	SJMP Light_CTRL
 STOP:
-    SJMP    STOP
-
-    END
+	SJMP	STOP
+	
+	
+END	
